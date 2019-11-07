edf334987c77f881b65c55bbec6769ce0ff788df
Mohamed <agamy207@gmail.com>
2019-11-07 12:59:18 +0200

    frist

--- app/src/main/java/com/rocket/a57357/auth/Login/Login.java
+++ app/src/main/java/com/rocket/a57357/auth/Login/Login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.rocket.a57357.R;
 import com.rocket.a57357.auth.SignUp.SignUp;
@@ -23,6 +24,7 @@ btn2.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
         startActivity(new Intent(Login.this, SignUp.class));
+        Toast.makeText(Login.this,"agamy",Toast.LENGTH_LONG).show();
     }
 });
 
